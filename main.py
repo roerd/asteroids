b9225516249edfad61f1167736a92711c3dd1f78
Rörd Hinrichsen <roerdhh@gmail.com>
2025-01-18 05:47:04 +0100

    Reduce empty lines in main.py.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 
 from constants import *
 
-
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -20,7 +19,6 @@ def main():
         pygame.display.flip()
         # pause for constant 60 fps
         dt = clock.tick(60) / 1000
-
 
 if __name__ == '__main__':
     main()
